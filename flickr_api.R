501f7c036c96d4b15a62318892484eb01d7262ec
Joe Cheng <joe@rstudio.com>
2016-01-18 21:57:14 -0800

    Randomize flickr photos

--- flickr_api.R
+++ flickr_api.R
@@ -5,14 +5,14 @@ api_key <- "4f9974ed9c4ba04650ae725d743c9986"
 flickr_photos_search_one <- function(api_key, query) {
   resp <- GET(
     sprintf(
-      "https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&api_key=%s&text=%s&safe_search=1&content_type=1&per_page=1&sort=relevance",
+      "https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&api_key=%s&text=%s&safe_search=1&content_type=1&per_page=10&sort=relevance",
       api_key,
       utils::URLencode(query)
     ),
     accept_json()
   )
   resp <- jsonlite::fromJSON(sub("^jsonFlickrApi\\((.*)\\)$", "\\1", rawToChar(resp$content)))
-  resp$photos$photo
+  resp$photos$photo[sample.int(nrow(resp$photos$photo), 1),]
 }
 
 flickr_photo_url <- function(photo) {
